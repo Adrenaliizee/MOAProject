d1fafacd26ed4472a3d92b536badc4bef6d5205a
Lars Velthuis <larsvelthuis@msn.com>
2015-11-13 14:56:42 +0100

    Merge branch 'master' of https://github.com/Adrenaliizee/MOAProject
    Conflicts:
    	.idea/modules.xml

--- app/src/main/java/nl/larsvelthuis/moaproject/MainActivity.java
+++ app/src/main/java/nl/larsvelthuis/moaproject/MainActivity.java
@@ -1,9 +1,9 @@
 package nl.larsvelthuis.moaproject;
 
-
 //Gemaakt door: Lars
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -16,11 +16,17 @@ public class MainActivity extends AppCompatActivity {
     ImageView lion;
     ImageView deer;
     //Variabele aanmaken voor de timer van splashscreen
-    private final int SPLASH_DISPLAY_LENGTH = 1000;
+    private final int SPLASH_DISPLAY_LENGTH = 3000;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Super coole material design Snackbar weergeven
+        //om te laten zien dat de applicatie geladen wordt.
+
+        Snackbar snackbar = Snackbar
+                .make(getApplicationContext(), "Welcome to AndroidHive", Snackbar.LENGTH_LONG);
+
         //imgViews toewijzen aan de variabelen.
         lion = (ImageView) findViewById(R.id.imgLion);
         //Images toevoegen aan de imageviews.
