1217c346d77a2e6182a7f09d4010a1745f0c795d
Lars Velthuis <larsvelthuis@msn.com>
2015-11-16 14:28:17 +0100

    Merge branch 'master' of https://github.com/Adrenaliizee/MOAProject
    Conflicts:
    	.idea/modules.xml

--- app/src/main/java/nl/larsvelthuis/moaproject/Hoofdmenu.java
+++ app/src/main/java/nl/larsvelthuis/moaproject/Hoofdmenu.java
@@ -1,13 +1,56 @@
 package nl.larsvelthuis.moaproject;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+/* gemaakt door: Lars
+   datum: 16-11-2015
+ */
 public class Hoofdmenu extends AppCompatActivity {
-
+    //Imageviews aanmaken.
+    ImageView logo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hoofdmenu);
+        //imgViews variabelen toewijzen aan de imgviews.
+        logo = (ImageView)findViewById(R.id.imgLogo);
+
+        //plaatje toevoegen aan de imageview
+        logo.setImageResource(R.drawable.logo);
+    }
+    //Methode exitApp aanmaken. Sluit de applicatie af.
+    public void exitApp(View v)
+    {
+        //alertdialog aanmaken die aan de gebruiker vraagt of hij/zij
+        //echt af wilt sluiten
+        AlertDialog.Builder dialog = new AlertDialog.Builder(Hoofdmenu.this);
+        dialog.setMessage("Close the application?");
+        dialog.setPositiveButton("No", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                //gebruiker klikte op cancel.
+                return;
+            }
+        });
+        dialog.setNegativeButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        //gebruiker klikte op ok, dus applicatie afsluiten.
+                        System.exit(0);
+                    }
+
+                });
+        //alertdialog
+        dialog.show();
+    }
+
+    public void newGame()
+    {
+
     }
 }
--- app/src/main/java/nl/larsvelthuis/moaproject/MainActivity.java
+++ app/src/main/java/nl/larsvelthuis/moaproject/MainActivity.java
@@ -1,38 +1,38 @@
 package nl.larsvelthuis.moaproject;
 
-//Gemaakt door: Lars
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.widget.ImageView;
 import android.os.Handler;
-
+/* gemaakt door: Lars
+   datum: 13-11-2015
+ */
 public class MainActivity extends AppCompatActivity {
 
     //Imageviews aanmaken.
     ImageView lion;
     ImageView deer;
+    ImageView logo;
     //Variabele aanmaken voor de timer van splashscreen
-    private final int SPLASH_DISPLAY_LENGTH = 3000;
+    private final int splashLength = 3000;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Super coole material design Snackbar weergeven
-        //om te laten zien dat de applicatie geladen wordt.
 
-        Snackbar snackbar = Snackbar
-                .make(getApplicationContext(), "Welcome to AndroidHive", Snackbar.LENGTH_LONG);
-
-        //imgViews toewijzen aan de variabelen.
+        //imgViews variabelen toewijzen aan de imgviews.
         lion = (ImageView) findViewById(R.id.imgLion);
+        deer = (ImageView) findViewById(R.id.imgDeer);
+        logo = (ImageView) findViewById(R.id.imgLogo);
         //Images toevoegen aan de imageviews.
         lion.setImageResource(R.drawable.lion);
-        deer = (ImageView) findViewById(R.id.imgDeer);
         deer.setImageResource(R.drawable.deer);
+        logo.setImageResource(R.drawable.logo);
 
         // Nieuwe Handler maken om de nieuwe activity te starten
         // Splash screen afsluiten na een paar seconden
@@ -44,6 +44,6 @@ public class MainActivity extends AppCompatActivity {
                 MainActivity.this.startActivity(mainIntent);
                 MainActivity.this.finish();
             }
-        }, SPLASH_DISPLAY_LENGTH);
+        }, splashLength);
     }
 }
